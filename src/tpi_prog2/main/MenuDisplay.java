72e7c357b39810defad961efe70b9351d2128c43
Nicolas <nicolasarrastia@gmail.com>
2025-11-18 17:29:12 -0300

    update menu display

--- src/tpi_prog2/main/MenuDisplay.java
+++ src/tpi_prog2/main/MenuDisplay.java
@@ -34,18 +34,20 @@ public class MenuDisplay {
      * 0. Salir
      */
     public static void mostrarMenuPrincipal() {
-        System.out.println("\n========= MENU =========");
+        System.out.println("\n-=-=-=-=-= LIBROS -=-=-=-=-=");
         System.out.println("1. Crear libro");
         System.out.println("2. Listar libros");
         System.out.println("3. Actualizar libro");
         System.out.println("4. Eliminar libro");
+        System.out.println("\n-=-=-=-=-= FICHAS -=-=-=-=-=");
         System.out.println("5. Crear ficha bibliográfica");
         System.out.println("6. Listar fichas bibliográficas");
         System.out.println("7. Actualizar ficha bibliográfica por ID");
         System.out.println("8. Eliminar ficha bibliográfica por ID");
+        System.out.println("\n-=-=-=-=-= OTROS -=-=-=-=-=");
         System.out.println("9. Actualizar ficha bibliográfica por ID de libro");
         System.out.println("10. Eliminar ficha bibliográfica por ID de libro");
-        System.out.println("0. Salir");
+        System.out.println("0. SALIR");
         System.out.print("Ingrese una opción: ");
     }
 }
